e5f3c1ba8acfef631645743921d87502bf481559
artemkopytko <kopytkoartem@gmail.com>
2017-11-10 14:09:39 +0200

    Added Mongoose & POST

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,17 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
+var Post = require("../models/post");
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
@@ -16,5 +27,26 @@ app.get('/posts', (req, res) => {
     }]
   )
 })
+
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
 
 app.listen(process.env.PORT || 8081)
